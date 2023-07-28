e5c234f14fe2e26111dfc44dc738f1d1b35a3337
Avinash Gurav <avinashgurav36@gmail.com>
2023-07-28 12:36:26 +0530

    Test purpose added

---
+++ Test.java
@@ -0,0 +1,16 @@
+package work.consreference;
+
+public class ConstrReference
+{
+
+    public static void main(String[] args)
+    {
+         AviInterface a = () - new Avinash();
+         a.getAvi();
+
+        AviInterface a2 = Avinashnew;
+        a2.getAvi(5);
+
+    }
+
+}
